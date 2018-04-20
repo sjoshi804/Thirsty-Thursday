e7f71d1715df0169394fc6090d91dd85b19fc37a
Siddharth Joshi <sjoshi804@gmail.com>
2018-04-19 18:42:25 -0700

    Imported JsonResponse

--- Organize/views.py
+++ Organize/views.py
@@ -5,7 +5,7 @@
 #------------------------
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
 
