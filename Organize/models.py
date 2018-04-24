9d6fc85fd979c4f0ed8fa436cc9d5bc016071cb9
Siddharth Joshi <sjoshi804@gmail.com>
2018-04-24 09:51:42 -0700

    Changed up models for User and Organize to create databse fields for Users, Operators, Organizers and Parties

--- Organize/models.py
+++ Organize/models.py
@@ -3,8 +3,16 @@ from django.db import models
 
 class Party(models.Model):
     createdAt = models.DateTimeField(auto_now_add = True)
-    name = models.CharField(max_length = 100, blank = False, default = "Thursty Party")
-    organizer = models.ForeignKey(User, related_name = 'parties', on_delete = models.CASCADE, null = False)
+    eventName = models.CharField(max_length = 100, blank = False, default = "Thursty Party")
+#   hostedBy = models.ForeignKey(User, related_name = 'parties', on_delete = models.CASCADE, null = False)
+    time = models.DateTimeField(null = False)
+    location = models.CharField(max_length = 100, blank = False, null = False)
+#   paidVenmo = models.ForeinKey or manyToMany?
+#   paidCash = models.ForeignKey or manyToMany?
+#   attended = models.ForeignKey or ManyToMany?
     class Meta:
-        ordering = ('createdAt',)
+        ordering = ('time',)
+
+
+ 
 
