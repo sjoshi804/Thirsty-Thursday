5dbb39787101955aed9293ad22c0f521f374044a
Siddharth Joshi <sjoshi804@gmail.com>
2018-04-23 22:41:24 -0700

    Added parties model in organizer

--- Organize/models.py
+++ Organize/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+
+class Party(models.Model):
+    createdAt = models.DateTimeField(auto_now_add = True)
+    name = models.CharField(max_length = 100, blank = False, default = "Thursty Party")
+    organizer = models.ForeignKey(User, related_name = 'parties', on_delete = models.CASCADE, null = False)
+    class Meta:
+        ordering = ('createdAt',)
+
