4c5612bc3e99e693660e347ba0ff1c6798560270
Siddharth Joshi <sjoshi804@gmail.com>
2018-05-08 08:53:05 -0700

    Url changes everywhere - try 2

--- User/urls.py
+++ User/urls.py
@@ -3,11 +3,11 @@
 # Author: Siddharth Joshi
 # Date Created: 05/08/18
 #------------------------                                                                                                                                                                                  
-from django.conf.urls import url
+from django.urls import re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from User import views
 
 urlpatterns = [
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
+   re_path(r'^users/$', views.UserList.as_view(), name='user-list'),
+   re_path(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view(), name='user-detail'),
 ]
