169d8b10248ea4cdf642a759d532f8ee09eea481
Siddharth Joshi <sjoshi804@gmail.com>
2018-05-07 18:57:10 -0700

    Changed up Party's views

--- Party/views.py
+++ Party/views.py
@@ -4,10 +4,26 @@
 # Date Created: 04/18/18
 #------------------------
 
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from Party.models import Party
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+ 
+from Party.serializers import PartySerializer
+ 
+ 
+class PartyList(generics.ListCreateAPIView):
+    queryset = Party.objects.all()
+    serializer_class = PartySerializer
+ 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+ 
+ 
+class PartyDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PartySerializer
+ 
+    def get_queryset(self):
+        return Party.objects.all().filter(user=self.request.user)
 
-# Create your views here.
 
-def index(request):
-    return JsonResponse({'Gautam': 'is lonely'})
