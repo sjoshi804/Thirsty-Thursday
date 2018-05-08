247534227df4e6561f9f3520ecb18c4b5a4e636a
Siddharth Joshi <sjoshi804@gmail.com>
2018-05-08 11:09:04 -0700

    Fixed up models (related_name and added status field to indicate status for party rather than adding in Organizer models)

--- Party/models.py
+++ Party/models.py
@@ -2,9 +2,10 @@ from django.db import models
 # Create your models here.
 
 class Party(models.Model):
+    
+    #Basic Details
     createdAt = models.DateTimeField(auto_now_add = True)
     eventName = models.CharField(max_length = 100, blank = False, default = "Thursty Party")
-    
     hostedBy = models.ForeignKey('User.Organizer', related_name = 'parties', on_delete = models.CASCADE, null = False)
     time = models.DateTimeField(null = False)
     location = models.CharField(max_length = 100, blank = False, null = False)
@@ -13,6 +14,9 @@ class Party(models.Model):
     attended = models.ManyToManyField('User.User', related_name = '%(class)s_attended')
     paidVenmo = models.ManyToManyField ('User.User', related_name = '%(class)s_paidVenmo')
     paidCash = models.ManyToManyField('User.User', related_name = '%(class)s_paidCash')
+
+    #Status
+    status = models.CharField(max_length = 20, blank = False, default = "Upcoming")
 
     class Meta:
         ordering = ('time',)
