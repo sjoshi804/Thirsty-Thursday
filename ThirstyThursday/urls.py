c4c27158de78dc0e1f37d76a7ec2e37ac553f5db
Siddharth Joshi <sjoshi804@gmail.com>
2018-05-11 12:53:31 -0700

    Regex fix for urls pt. 2

--- ThirstyThursday/urls.py
+++ ThirstyThursday/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'party\/*', include('Party.urls')),
+    re_path(r'party/', include('Party.urls')),
 ]
