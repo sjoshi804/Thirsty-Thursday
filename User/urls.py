7e81254935b680a12912651b13a7a62e86b76b3b
Siddharth Joshi <sjoshi804@gmail.com>
2018-05-11 18:16:24 -0700

    Added app name in User.urls

--- User/urls.py
+++ User/urls.py
@@ -6,9 +6,9 @@
 from django.urls import re_path
 from rest_framework.urlpatterns import format_suffix_patterns
 from User import views
-
-urlpatterns = ([
+app_name = 'User'
+urlpatterns = [
    re_path(r'all/$', views.UserList.as_view(), name='user-list'),
    re_path(r'search/(?P<id>)/$', views.UserDetail.as_view(), name='user-detail'),
    re_path(r'filter/college/(?P<college>)/$', views.UserCollege.as_view(), name='user-college-detail'),
-], 'User')
+]
