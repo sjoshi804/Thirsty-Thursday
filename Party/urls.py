741ef71474702565c089662c26951eb6026e896b
Siddharth Joshi <sjoshi804@gmail.com>
2018-05-25 17:01:32 -0700

    trying to fix git

--- Party/urls.py
+++ Party/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     re_path(r'search/(?P<partyid>.+)/$', views.PartyDetail.as_view(), name='party-detail'),
     re_path(r'filter/college/(?P<partyid>.+)/$', views.PartyManyDetail.as_view(), name='party-college-details'),
     re_path(r'filter/organized-by/(?P<partyid>.+)/$', views.PartyManyDetail.as_view(), name='party-organizer-details'),
-    re_path(r'checkin/(?P<partyid>.+)/(?P<guest>.+)/$', views.PartyCheckIn, name = 'party-check-in')
+
 ]
