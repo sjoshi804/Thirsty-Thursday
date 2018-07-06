da6a7aafa1b5323b6cddfbc4692487d11cd5d6b5
Siddharth Joshi <sjoshi804@gmail.com>
2018-07-06 22:51:05 +0530

    Added Status field - set default to interested

--- Guest/models.py
+++ Guest/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Guest(models.Model):
+    #Status
+    status = models.CharField(default = "Interested", max_length = 50, blank = True)
+    
     #Unique Identifiers
     guestInstanceID = models.CharField(null = False, blank = False, max_length = 50, unique = True, primary_key = True)
     partyID = models.CharField(null = False, blank = False, max_length = 20)
@@ -13,5 +16,5 @@ class Guest(models.Model):
 
     #Guest Instance Specific Information
     entryTime = models.DateTimeField(blank = True, null = True)
-    exitTime = models.DateTimeField(blank = True, null = True)
+    exitTime = models.DateTimeField(blank = True, null = True, default = "1970-01-01T00:00:00Z")
     paymentMethod = models.CharField(default = "Not Paid", max_length = 50, blank = True)
